751add90af12e905112dfa14ac522d4348a12d4b
YTeH0k <denikpro@mail.ru>
2020-05-19 02:03:54 +0300

    create DTO models 'User' and 'Role'

---
+++ adminka/Model/RoleDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace adminka.Model
+{
+    public class RoleDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Access { get; set; }
+
+        public UserDTO UserDTO { get; set; }
+    }
+}
---
+++ adminka/Model/UserDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace adminka.Model
+{
+    public class UserDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public string UserName { get; set; }
+
+        [Key]
+        [ForeignKey("Role")]
+        public int RoleId { get; set; }
+        public RoleDTO RoleDTO { get; set; }
+
+    }
+}
